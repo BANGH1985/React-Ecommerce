252a8bb9c038835a31bdbce86cbeb29a5dad80e0
Dante Soriano <62659493+BANGH1985@users.noreply.github.com>
2023-11-03 16:49:05 -0300

    arreglos estilo estetica

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom"
 import { Itemcounter } from "../itemCounter/itemCounter"
 import { Button, Card, ListGroup } from "react-bootstrap"
 import { mFetch } from "../../helpers/mFetch"
+import "./ItemDetailContainer.css"
 
 
 
@@ -20,21 +21,23 @@ export const ItemDetailContainer = () => {
     }
 
     return (    
-        <div className="card w-25 h-100 ">   
-            <Card.Img  variant="top" src={product.image} />
-            <Card.Body>
-                <Card.Title>{product.name}</Card.Title>
-            </Card.Body>
-            <ListGroup className="list-group-flush">
-                {product.category}
-                <br />
-                {product.price}
-            </ListGroup>
-            <Card.Body>
-                <Itemcounter initial={1} stock={10} onAdd={onAdd}/>
-                <br />
-                <Button variant="dark">Comprar</Button>
-            </Card.Body>
+        <div className="container-fluid cardDetail">   
+            <div className="card w-25 h-100">   
+                <Card.Img  variant="top" src={product.image} />
+                <Card.Body>
+                    <Card.Title>{product.name}</Card.Title>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                    {product.category}
+                    <br />
+                    Precio: ${product.price}
+                </ListGroup>
+                <Card.Body>
+                    <Itemcounter initial={1} stock={10} onAdd={onAdd}/>
+                    <br />
+                    <Button variant="dark">Comprar</Button>
+                </Card.Body>
+            </div>
         </div>
     )
 }
--- src/components/itemList/ItemList.jsx
+++ src/components/itemList/ItemList.jsx
@@ -17,11 +17,13 @@ const prouctFiltered = ({products, filterState, handleFilterChange}) => (
             filterState === '' ? 
                                     products.map(product =>
                                         <div key={product.id} className="card w-25 estiloCard">
-                                            <img src={product.image} className="card-img-top flex timg" alt=""/>
+                                            <div className='card-img-top contenedorImagen'>   
+                                                <img src={product.image} className="card-img-top flex timg" alt=""/>
+                                            </div>
                                             <div className="card-body description">
                                                 <h3>{product.name}</h3>
                                                 <p>{product.category}</p>
-                                                <p>Precio: {product.price}</p>
+                                                <p>Precio: ${product.price}</p>
                                                 <Link to={`/detail/${product.id}`} className="card-footer">
                                                     <button className="btn-dark w-100">Detalle</button>
                                                 </Link>
@@ -37,7 +39,7 @@ const prouctFiltered = ({products, filterState, handleFilterChange}) => (
                                                 <div className="card-body">
                                                     <h3>{product.name}</h3>
                                                     <p>{product.category}</p>
-                                                    <p>Precio: {product.price}</p>
+                                                    <p>Precio: ${product.price}</p>
                                                     <Link to={`/detail/${product.id}`} className="card-footer">
                                                         <button className="btn-dark w-100">Detalle</button>
                                                     </Link>
